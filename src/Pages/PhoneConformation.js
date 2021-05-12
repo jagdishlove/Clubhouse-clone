c25f2028b4a8e36f531b4fcdf376a4c608578a88
jagdishlove <jagdish.funn@gmail.com>
2021-05-12 15:33:50 +0530

    first

---
+++ src/Pages/PhoneConformation.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import style from "../style/welcome.module.css";
+
+function PhoneConformation() {
+  const [value, setValue] = useState();
+
+  return (
+    <div className={style.phoneConfirmContainer}>
+      <h1>Enter your Phone</h1>
+      <p>
+        By entering your number, your're agreeing to put{" "}
+        <spam>Term of Service and Policy. </spam>
+         Thanks!
+      </p>
+    </div>
+  );
+}
+
+export default PhoneConformation;
---
+++ src/Pages/Welcome.js
@@ -0,0 +1,34 @@
+import React from "react";
+import style from "../style/welcome.module.css";
+import { Link } from "react-router-dom";
+
+export default function Welcome() {
+  return (
+    <div className={style.WelcomeContainer}>
+      <h1>Welcome!</h1>
+      <div className={style.WelcomeInfo}>
+        <p>
+          Clubhouse is an invitation-only social media app for iOS and Android
+          that facilitates auditory communication through rooms that can
+          accommodate groups of up to 5,000 people.
+        </p>
+        <p>
+          Clubhouse is an invitation-only social media app for iOS and Android
+          that facilitates auditory communication through rooms that can
+          accommodate groups of up to 5,000 people.
+        </p>
+        <p>Jagdish, mehra Clubhouse Team</p>
+      </div>
+      <div className={style.actionBtn}>
+        <Link
+          exact
+          to="/get_username"
+          className="primaryBtn d-flex align-items-center mb-3"
+          
+        >Get your username </Link>
+
+        <Link>Have an invite text? Sign in</Link>
+      </div>
+    </div>
+  );
+}
