f5e0393114b39b7bbde170582511409ac183eab7
jagdishlove <jagdish.funn@gmail.com>
2021-05-13 10:02:47 +0530

    changes

--- src/Pages/PhoneConformation.js
+++ src/Pages/PhoneConformation.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import style from "../style/welcome.module.css";
+import style from "../style/phoneConfirm.module.css";
 
 function PhoneConformation() {
   const [value, setValue] = useState();
