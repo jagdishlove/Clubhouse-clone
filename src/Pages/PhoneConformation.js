d638716311b80485b4a485959e5a6e7085fed215
jagdishlove <jagdish.funn@gmail.com>
2021-05-13 14:39:48 +0530

    Button

--- src/Pages/PhoneConformation.js
+++ src/Pages/PhoneConformation.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PhoneInput from "react-phone-number-input";
 import { Link } from "react-router-dom";
 import style from "../style/phoneConfirm.module.css";
 
@@ -7,12 +8,29 @@ function PhoneConformation() {
 
   return (
     <div className={style.phoneConfirmContainer}>
+      <Link exact to="/" className={style.backBttn}>
+        <img src="/images/arrow.png" alt="" />
+      </Link>
       <h1>Enter your Phone</h1>
+      <PhoneInput
+        international
+        defaultCountry="IN" 
+        value={value}
+        onChange={setValue}
+      />
       <p>
         By entering your number, your're agreeing to put{" "}
-        <spam>Term of Service and Policy. </spam>
-         Thanks!
+        <span>Term of Service and Policy. </span>
+        Thanks!
       </p>
+      <Link
+        exact
+        to="/code_confirm"
+        className="primaryBtn d-flex align-items-center"
+      >
+        Next 
+        <img src="/images/nextArrowIcon.svg" className="" alt=" " />
+      </Link>
     </div>
   );
 }
