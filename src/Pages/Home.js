791b1b4570de3a081e4e8357f3f06f1916af9dfa
jagdishlove <jagdish.funn@gmail.com>
2021-06-23 23:27:35 +0530

    buttons

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -18,6 +18,22 @@ function Home() {
 
   return (
     <div>
+      {loaderVisibility ? (
+        <div style={{
+          position: 'fixed',
+          top: '0',
+          right: '0',
+          bottom: '0',
+          left: '0',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <img src="images/loader.gif" alt="" />
+        </div>
+      ) : (
+        " "
+      )}
       <Header />
       <div className={style.home_container}>
 
@@ -37,16 +53,16 @@ function Home() {
         sheetTitle='start room'
         setSheetVisible={(item) => setSheetVisible(item)}
         sheetVisible={sheetVisible}
-        cardDetail={data.find((item)=> item.id ==cardId)}
-      setItemVisible={(item) => setItemVisible(item)}
-      setSheetCreateRoom={(item) => {
-        setLoaderVisibility(true);
-        setTimeout(() => {
-          setSheetCreateRoom(item);
-          setLoaderVisibility(false);
-        }, 1000);
-      }}
-          />
+        cardDetail={data.find((item) => item.id == cardId)}
+        setItemVisible={(item) => setItemVisible(item)}
+        setSheetCreateRoom={(item) => {
+          setLoaderVisibility(true);
+          setTimeout(() => {
+            setSheetCreateRoom(item);
+            setLoaderVisibility(false);
+          }, 1000);
+        }}
+      />
     </div>
   )
 }
