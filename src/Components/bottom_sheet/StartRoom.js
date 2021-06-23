f3f1cbed0716d3d3dcb09c36570026a42e338871
jagdishlove <jagdish.funn@gmail.com>
2021-06-23 13:39:35 +0530

    Button

--- src/Components/bottom_sheet/StartRoom.js
+++ src/Components/bottom_sheet/StartRoom.js
@@ -1,9 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import style from '../../style/bottomSheet.module.css';
-import {FcGlobe} from "react-icons/fc";
+import { FcGlobe } from "react-icons/fc";
 
 function StartRoom(props) {
-    const[room, setRoom]= useState("open");
+    const [room, setRoom] = useState("open");
     return (
         <>
             <div className={style.switch_Line}></div>
@@ -11,30 +11,43 @@ function StartRoom(props) {
                 <button className={style.addTopicBtn}>+ add a topic</button>
             </div>
             <div className={style.selectRoom}>
-                <button className={room =='open' ? style.active:""}
-                onClick={()=>setRoom("open")}
+                <button className={room == 'open' ? style.active : ""}
+                    onClick={() => setRoom("open")}
                 >
                     <div>
-                        <FcGlobe/>
-                        
+                        <FcGlobe />
+
                     </div>
                     Open
                 </button>
-                <button className={room =="social" ? style.active:""}
-                onClick={()=> setRoom("social")}>
+                <button className={room == "social" ? style.active : ""}
+                    onClick={() => setRoom("social")}>
                     <div>
-                        <FcGlobe/>
+                        <FcGlobe />
                     </div>
                     Social
                 </button>
-                <button className={room =="closed" ? style.active:""}
-                onClick={()=> setRoom("closed")}>
+                <button className={room == "closed" ? style.active : ""}
+                    onClick={() => setRoom("closed")}>
                     <div>
-                        <FcGlobe/>
+                        <FcGlobe />
                     </div>
                     Closed
                 </button>
             </div>
+            <p>Start a room <span>{room=="closed" 
+                ? "for people i choose" 
+                :room =="social"
+                ?"with people I follow"
+                :"open to everyone"}
+                </span></p>
+                <div  className="text-center">
+                    <button className={style.letGoBtn}
+                    onClick={()=> {
+                        props.setSheetCreateRoom(true);
+                        props.setSheetVisible(true);
+                    }}>Let's Go</button>
+                </div>
         </>
     )
 }
